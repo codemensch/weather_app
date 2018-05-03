c425f1982aa22ef985a70122adbc7e2bccf82568
codemensch <codemensch@gmail.com>
2018-05-03 04:50:20 +0000

    Fixed API bugs, added gradient background, added code comments.

--- public/main.js
+++ public/main.js
@@ -1,171 +1,134 @@
 $(document).ready(function(){
+    //Hide celsius temp and symbol
+    $('#celsius, #degreeC').hide();
+    
+    //Get current geolocation from browser
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(function(position){
             
             var lat = position.coords.latitude;
             var lon = position.coords.longitude;
             
+            //Get current weather data from Openweathermap using lat and lon in JSON format
             $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=3b9d5ed397dc75ae87dadd2e65881d68', function(json){
                 
+                //Get code for current weather condition
                 var code = json.weather[0].id;
-                console.log(code);
+                
                 $.getJSON('/weatherIcons.json', function(iconsJson){
-                    
                     var prefix = 'wi wi-';
+                    
+                    //Lookup condition code in Icons JSON
                     var icon = iconsJson[code].icon;
                     var iconAlt = iconsJson[code].iconAlt;
+                    
+                    //Get current time in UTC as timestamp in seconds
                     var dateUTC = new Date();
-                    console.log(dateUTC);
                     var timestamp = dateUTC.getTime() / 1000;
                     
                     //Current Time/Date
                     $.getJSON('https://maps.googleapis.com/maps/api/timezone/json?location=' + lat + ',' + lon + '&timestamp=' + timestamp + '&key=AIzaSyDn8-mFDgFAt9XUTmFHkLproXkEJUHusPI', function(googleJson){
-                        //Get current time in current location
+                        //Factor in daylight savings and time zone
                         var dstOffset = googleJson.dstOffset;
-                        console.log(dstOffset);
                         var rawOffset = googleJson.rawOffset;
-                        console.log(rawOffset);
                         var currentTime = ((timestamp + dstOffset + rawOffset) * 1000); // Current date/time of user computer expressed as seconds since midnight, January 1, 1970 UTC
-                        console.log(currentTime);
                         var currentDate = new Date(currentTime);
-                        console.log(currentDate);
-                        var dateString = currentDate.toISOString();
-                        console.log(dateString);
-                        dateString = dateString.slice(0, 10);
-                        console.log(dateString);
+                        
                         
                         
                         $.getJSON('https://api.apixu.com/v1/forecast.json?key=0273729a39cc4f93bc5172633182104&q=' + lat + ',' + lon, function(sunriseSunset){
+                            //Get year, month, and day of sunrise/sunset
                             var sunRiseSetYear = currentDate.getFullYear();
                             var sunRiseSetMonth = currentDate.getMonth();
                             var sunRiseSetDay = currentDate.getDate();
-                            console.log(sunriseSunset);
-                            //Sunrise from APIXU
+                            
+                            //Get sunrise time from APIXU
                             var sunrise = sunriseSunset.forecast.forecastday[0].astro.sunrise;
-                            console.log(sunrise);
+                            
+                            //Slice hour and minute from time
                             var sunriseHour = Number(sunrise.slice(0,2));
-                            console.log(sunriseHour);
                             var sunriseMinute = Number(sunrise.slice(3,5));
-                            console.log(sunriseMinute);
+                            
+                            //Create new timestamp for sunrise
                             var sunriseTimestamp = new Date(Date.UTC(sunRiseSetYear, sunRiseSetMonth, sunRiseSetDay, sunriseHour, sunriseMinute));
                             sunriseTimestamp = sunriseTimestamp.getTime();
-                            console.log(sunriseTimestamp);
                             
-                            //Sunset from APIXU
+                            //Get sunset time from APIXU
                             var sunset = sunriseSunset.forecast.forecastday[0].astro.sunset;
-                            console.log(sunset);
+                            
+                            //Slice hour and minute from time
                             var sunsetHour = Number(sunset.slice(0,2)) + 12;
-                            console.log(sunsetHour);
                             var sunsetMinute = Number(sunset.slice(3,5));
-                            console.log(sunsetMinute);
+                            
+                            //Create new timestamp for sunset
                             var sunsetTimestamp = new Date(Date.UTC(sunRiseSetYear, sunRiseSetMonth, sunRiseSetDay, sunsetHour, sunsetMinute));
                             sunsetTimestamp = sunsetTimestamp.getTime();
-                            console.log(sunsetTimestamp);
                             
+                            //Choose day/night icon by comparing local time with time of sunrise/sunset
                             if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+                                
                                 $('#condition').className = '';
+                                var dayNight = '';
+                                
+                                //Change sun to moon icon for nightime clear conditions only
                                 if (code == 800){
                                     if (currentTime >= sunriseTimestamp && currentTime < sunsetTimestamp){
                                         icon = 'day-' + icon;
                                         icon = prefix + icon;
-                                        console.log(icon);
                                         $('#condition').addClass(icon);
+                                        dayNight = 'day';
                                     }
                                     else {
                                         iconAlt = 'night-' + iconAlt;
                                         icon = prefix + iconAlt;
-                                        console.log(icon);
                                         $('#condition').addClass(icon);
+                                        dayNight = 'night';
                                     }
                                 }
                                 else if (currentTime >= sunriseTimestamp && currentTime < sunsetTimestamp){
                                     icon = 'day-' + icon;
+                                    dayNight = 'day';
                                 }
                                 else {
                                     icon = 'night-alt-' + icon;
+                                    dayNight = 'night';
                                 }
 
                                 icon = prefix + icon;
-                                console.log(icon);
+                                
                                 $('#condition').addClass(icon);
+                                
+                                //Change background color for night and day
+                                if (dayNight == 'day'){
+                                    $('body').css('background', 'linear-gradient(#97ccf1, #ffa500)');
+                                }
+                                else {
+                                    $('body').css('background', 'linear-gradient(#97ccf1, #330033)');
+                                }
                             }
                             
                             else {
                                 icon = prefix + icon;
-                                console.log(icon);
                                 $('#condition').addClass(icon);
                             }
+                            
+                            //Load Current city/state
+                            $('#city').html(sunriseSunset.location.name + ', ' + sunriseSunset.location.region);
+                            
+                            //Load current temp in F and C, hide C on page load
+                            var degreeF = Math.round(sunriseSunset.current.feelslike_f);
+                            $('#farenheit').html(degreeF);
+                            $('#degreeF').html('&degF');
+                            var degreeC = Math.round(sunriseSunset.current.feelslike_c);
+                            $('#celsius').html(degreeC);
+                            $('#degreeC').html('&degC');
+                            $('#degreeC, #degreeF').click(function(){
+                            $('#degreeC, #degreeF, #celsius, #farenheit').toggle();
+                            });
                         });
-                        
-                        /*
-                        // If we are not in the ranges mentioned above, add a day/night prefix.
-                        if (code == 800){
-                            if (currentTime >= sunrise && currentTime < sunset){
-                                icon = 'day-' + icon;
-                                icon = prefix + icon;
-                                console.log(icon);
-                                $('#condition').removeClass().addClass(icon);
-                            }
-                            else {
-                                iconAlt = 'night-' + iconAlt;
-                                // Finally tack on the prefix.
-                                icon = prefix + icon;
-                                console.log(icon);
-                                $('#condition').removeClass().addClass(iconAlt);
-                            }
-                        }
-                        
-                        if (!(code > 699 && code < 801) && !(code > 899 && code < 1000)) {
-                            if (currentTime >= sunrise && currentTime < sunset){
-                                icon = 'day-' + icon;
-                            }
-                            else {
-                                icon = 'night-alt-' + icon;
-                            }
-    
-                            // Finally tack on the prefix.
-                            icon = prefix + icon;
-                            console.log(icon);
-                            $('#condition').removeClass().addClass(icon);
-                        }
-                        */
                     });
                 });
-                $('#city').html(json.name);
             });
-            
-            
-            
-            /*
-            var weatherIcons = $.getJSON('weatherIcons.json', function(weatherJson){});
-            var lat = position.coords.latitude;
-            var lon = position.coords.longitude;
-            var req = $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=3b9d5ed397dc75ae87dadd2e65881d68', function(json){
-                console.log(lat);
-                console.log(lon);
-                console.log(req);
-                $('#city').html(json.name);
-            });
-            
-            req.then(function(resp) {
-              var prefix = 'wi wi-';
-              var code = resp.weather[0].id;
-              console.log(code);
-              var icon = weatherIcons[code].icon;
-            
-              // If we are not in the ranges mentioned above, add a day/night prefix.
-              if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
-                icon = 'day-' + icon;
-              }
-            
-              // Finally tack on the prefix.
-              icon = prefix + icon;
-              console.log(icon);
-              $('#condition').removeClass().addClass(icon);
-            });
-            
-            */
-            
         });
     }
 });
